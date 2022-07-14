2d193a1628807b8aca8808dc71c61ed36d92b7b4
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-14 11:47:27 -0400

    Notes and labs on strings

---
+++ strings-basics.js
@@ -0,0 +1,24 @@
+const message = 'Hello Javascript Students'
+
+const msgToLowerCase = message.toLowerCase()
+const msgToUpperCase = message.toUpperCase()
+const messageNoSpaces = message.trim()
+const newMessage = message.replace('Students', 'Engineers')
+const withSubString = message.substring(9, 14)
+
+
+console.log(message)
+console.log(msgToLowerCase)
+console.log(msgToUpperCase)
+console.log(messageNoSpaces)
+console.log(newMessage)
+console.log(withSubString)
+
+const mySchool = 'Boca Code'
+const myIndex = mySchool.indexOf('Code')
+console.log(myIndex)
+
+const myString = 'Hello level up students!'
+const newString = myString.split(' l ')
+console.log(newString.reverse())
+
